ada77d6bb745d6c9a340d78454f5ddd57899ac0d
edua152030 <eduardo.vaz.faria@gmail.com>
2022-10-13 12:45:26 -0300

    controle

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,14 +4,28 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  //estado da aplicação
+  //alterado por mutations
+  // alteração de state geram renders
   state: {
+    nome: 'eduardo',
+    empresa: 'divimed',
+    logado: false
   },
+  //objeto de leitura da aplicação
   getters: {
   },
+  //alteração no estado da aplicação - somente devem ocorrer aletações aqui
+  //execução de forma sincrona
+  //acionado atraves de um commit
   mutations: {
   },
+  //ação realizada atraves de um dispatch
+  //pode executar tarefas assincronas
+  //encarregado de disparar mutaitions 
   actions: {
   },
+  //costruir  divisoes dentro da store
   modules: {
   }
 })
